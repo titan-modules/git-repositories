43cae93843e4c742a27f4ec4a845d5f3ca2ebde3
Mike Mackintosh <m@splug.io>
2014-06-14 20:03:33 -0400

    changed output format

--- analyze_git_repositories.py
+++ analyze_git_repositories.py
@@ -11,74 +11,75 @@ import re
 import json
 import logging
 import subprocess
-from sys import argv
-from os import chmod
+# from sys import argv
 from time import time, gmtime, strftime
-from os.path import isfile,dirname,basename
+from os.path import dirname,basename
 
 # Set Logging Status
 logging_enabled = False
 
 def run_command( command ):
-	ps = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-	output = ps.communicate()[0]
-	return output
+  ps = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+  output = ps.communicate()[0]
+  return output
 
 class AnalyzeGitCheckouts(object):
     """ AnalyzeGitCheckouts """
 
     def __init__(self):
-        self.repos = {}
+        self.repos = []
 
     def check_git_dirs(self):
         """
         Log all loaded kernel extensions
         """
         repos = run_command('find /Users -type d -name ".git"').splitlines()
-       	for repo in repos:
 
-       		repo_path = dirname(repo)
-       		repo_git_path = repo
-       		repo_name = basename(dirname(repo))
+        for repo in repos:
 
-       		if logging_enabled:
-       			print "Found '%s' @ '%s'" % (repo_name, repo_path)       		
+          repo_path = dirname(repo)
+          repo_git_path = repo
+          repo_name = basename(dirname(repo))
 
-       		self.repos[repo_path] = { 
-       			'name': repo_name, 
-       			'git_path': repo_git_path, 
-	       		'branches': {
-	       			'remote': [], 
-	       			'local': [] 
-	       		},
-       		}
-       		
-       		local_branches = run_command("git --git-dir=%s --work-tree=%s branch" % (repo_git_path, repo_path))
-       		for branch in local_branches.splitlines():
+          if logging_enabled:
+            print "Found '%s' @ '%s'" % (repo_name, repo_path)           
 
-       			self.repos[repo_path]['branches']['local'].append(branch.lstrip())
+          # Get latest commit
+          most_recent_commit = run_command('git --git-dir="%s" --work-tree="%s"  log --pretty=format:"%%cd|[Commit: %%h] [Date: %%cd] [Commiter: %%cn] [Commiter Email: %%ce] [Message: %%s]" -1' % (repo_git_path, repo_path)).split("|", 1)
 
-       		remote_branches = run_command("git --git-dir=%s --work-tree=%s branch -r" % (repo_git_path, repo_path))
-       		for branch in remote_branches.splitlines():
-	   			self.repos[repo_path]['branches']['remote'].append(branch.lstrip())
+          commit_date = most_recent_commit[0]
+          commit_details = most_recent_commit[1] 
+           
+          # Get remote
+          remotes = run_command("git --git-dir=%s --work-tree=%s remote -v" % (repo_git_path, repo_path))
 
-       		most_recent_commit = run_command("git --git-dir=%s --work-tree=%s log -1 --oneline" % (repo_git_path, repo_path))
-       		self.repos[repo_path]['commit'] = most_recent_commit
+          # Append to master
+          self.repos.append({
+              "name": repo_name,
+              "path": repo_path,
+              "date": commit_date,
+              "last_commit": commit_details,
+              "remotes": remotes,
+            })
+
+        # Set Message
+        self.message = "Found %d repositories" % len(self.repos)
+
+        # If no issues, return 0
+        self.status = 0
 
     def analyze(self):
         """
         This is the 'main' method that launches all of the other checks
         """
         self.check_git_dirs()
-        return json.dumps(self.repos)
+
+        return json.JSONEncoder().encode({"status": self.status, "message": self.message,"response": self.repos})
 
 
 if __name__ == "__main__":
 
     start = time()
-
-    # the "exec_date" is used as the "date" field in the datastore
-    exec_date = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
 
     ###########################################################################
     # Gather data
@@ -95,7 +96,7 @@ if __name__ == "__main__":
 
     # to see how long this module took to execute, launch the module with
     # "--log" as a command line argument
-    if "--log" in argv[1:]:
-    	logging_enabled = True
-        logging.basicConfig(format='%(message)s', level=logging.INFO)
-    logging.info("Execution took %s seconds.", str(end - start))
+    #if "--log" in argv[1:]:
+    #  logging_enabled = True
+    #  logging.basicConfig(format='%(message)s', level=logging.INFO)
+    #  logging.info("Execution took %s seconds.", str(end - start))
